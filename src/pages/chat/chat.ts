22b282a3667ecec9a307392b62f0afeb02a34a44
Guilherme Alves Sotero <840564@sga.pucminas.br>
2018-11-13 20:27:47 -0200

    Alteracao 13-11

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../providers/auth/auth.service';
 import { UserService } from '../../providers/user/user.service';
+import { User } from '../../models/user.model';
 
 
 
@@ -14,19 +15,33 @@ import { UserService } from '../../providers/user/user.service';
 export class ChatPage {
 
   messages: string[] = [];
+  pageTitle: string;
+  sender: User;
+  recipient: User;
 
   constructor(
     public authService: AuthService,
     public navCtrl: NavController,
     public navParams: NavParams,
     public userService: UserService
+
         ) {
   }
 
 
 
+
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ChatPage');
+    this.recipient = this.navParams.get('recipientUser');
+    this.pageTitle = this.recipient.name;
+    this.userService
+      .mapObjectKey<User>(this.userService.currentUser)
+      .first()
+      .subscribe((currentUser: User) => {
+        this.sender = currentUser;
+
+      });
+
   }
 
   sendMessage(newMessage: string): void {
